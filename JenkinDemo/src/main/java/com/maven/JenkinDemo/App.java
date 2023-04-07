8ea287df5adee678e3b8d8b4f382a0cb63364e22
suyash-metha <98865988+suyash-metha@users.noreply.github.com>
2023-04-07 15:24:56 +0530

    jenkinDemo

---
+++ JenkinDemo/src/main/java/com/maven/JenkinDemo/App.java
@@ -0,0 +1,13 @@
+package com.maven.JenkinDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
