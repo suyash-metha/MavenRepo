47f5aadbeb892dfebb6e3e0bbc7726c173cd9f1d
suyash-metha <98865988+suyash-metha@users.noreply.github.com>
2023-04-08 14:28:33 +0530

    change in code

--- JenkinDemo/src/main/java/com/maven/JenkinDemo/App.java
+++ JenkinDemo/src/main/java/com/maven/JenkinDemo/App.java
@@ -4,10 +4,12 @@ package com.maven.JenkinDemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public static void Product() {
+		System.out.println("Thank You");
+	}
 }
